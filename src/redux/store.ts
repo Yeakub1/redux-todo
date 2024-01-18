13aee8e1f3f7439c3aaacf049e87aa789ba9b285
Yeakub1 <mryeakub10@gmail.com>
2024-01-18 16:46:03 +0600

    connent store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import todoReducer from './features/todoSlice'
 
 export const store = configureStore({
-    reducer: {}
+    reducer: {todos: todoReducer}
 });
 
 export type RootState = ReturnType<typeof store.getState>
