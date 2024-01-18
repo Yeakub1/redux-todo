3520e7eb7ff5fd5a9da5efb078d91346c991a528
Yeakub1 <mryeakub10@gmail.com>
2024-01-18 16:29:06 +0600

    setup the project and start the code

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Todo app</h1>
+      <Button>my button</Button>
+    </div>
+  );
+};
+
+export default App;
